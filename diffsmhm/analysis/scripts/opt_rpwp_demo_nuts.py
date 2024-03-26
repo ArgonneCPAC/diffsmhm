8103af2697f269ca6cd6699be45aca3576822b0a
Joseph Wick <josephwick@outlook.com>
2024-03-25 19:22:11 -0700

    Update opt_rpwp_demo_nuts.py

--- diffsmhm/analysis/scripts/opt_rpwp_demo_nuts.py
+++ diffsmhm/analysis/scripts/opt_rpwp_demo_nuts.py
@@ -43,25 +43,28 @@ from diffsmhm.analysis.tools.rpwp import compute_rpwp
 
 from diffsmhm.analysis.tools.error import mse_rpwp
 
+from hmc_bounding import (
+    hmc_pos_to_model_pos,
+    logdens_model_to_logdens_hmc
+)
 
 # data files and params
-#halo_file="/home/jwick/data/value_added_orphan_complete_bpl_1.002310.h5"
-#particle_file="/home/jwick/data/hlist_1.00231.particles.halotools_v0p4.hdf5"
-halo_file="/Users/josephwick/Documents/Argonne23/data/value_added_orphan_complete_bpl_1.002310.h5"
-particle_file="/Users/josephwick/Documents/Argonne23/data/hlist_1.00231.particles.halotools_v0p4.hdf5"
-box_length = 250.0 # Mpc
-buff_wprp = 20.0 # Mpc
+halo_file = "/home/jwick/data/value_added_orphan_complete_bpl_1.002310.h5"
+particle_file = "/home/jwick/data/hlist_1.00231.particles.halotools_v0p4.hdf5"
+box_length = 250.0  # Mpc
+buff_wprp = 20.0  # Mpc
 
-outdir = "/home/jwick/diffsmhm/analysis/output/rpwp_demo_nuts"
+outdir = "/home/jwick/diffsmhm/diffsmhm/analysis/scripts/output/rpwp_demo_nuts"
 
 mass_bin_edges = np.array([10.6, 11.2], dtype=np.float64)
 
 rpbins = np.logspace(-1, 1.2, 13, dtype=np.float64)
-zmax = 20.0 # Mpc
+zmax = 20.0  # Mpc
 
-theta = np.array(list(smhm_params.values()) + 
-                 list(smhm_sigma_params.values()) + 
-                 list(disruption_params.values()), dtype=np.float64
+theta = np.array(
+            list(smhm_params.values()) +
+            list(smhm_sigma_params.values()) +
+            list(disruption_params.values()), dtype=np.float64
 )
 theta_init = np.copy(theta)
 
@@ -69,8 +72,8 @@ n_params = len(theta)
 n_rpbins = len(rpbins) - 1
 
 # hmc params
-n_warmup_steps = 1
-n_iter = 1
+n_warmup_steps = 500
+n_iter = 1000
 #
 
 lower_bounds = np.array([
@@ -112,7 +115,7 @@ halos, _ = load_and_chop_data_bolshoi_planck(
                 halo_file,
                 box_length,
                 buff_wprp,
-                host_mpeak_cut=14.7
+                host_mpeak_cut=0.0
 )
 
 idx_to_deposit = _calculate_indx_to_deposit(halos["upid"], halos["halo_id"])
@@ -156,26 +159,7 @@ if RANK == 0:
 
 # define functions
 
-# transform functions; following STAN manual for bounded scalar
-
-def logit(x):
-    return jax.scipy.special.logit(x)
-
-def logit_inv(x):
-    return jax.nn.sigmoid(x)
-
-def logit_inv_jac(x):
-    sig = jax.nn.sigmoid(x)
-    return sig * (1 - sig)
-
-def transform_model_to_hmc(x, a, b):
-    return logit((x - a) / (b - a))
-
-def transform_hmc_to_model(y, a, b):
-    return a + (b - a) * logit_inv(y)
-
-# logdensity function set upi
-# note that only rank zero will deal with this func
+# logdensity function set up; only RANK 0 deals with this
 def logdensity_fn(
     *,
     smhm_logm_crit, smhm_ratio_logm_crit, smhm_k_logm, smhm_lowm_index,
@@ -201,29 +185,31 @@ def logdensity_fn(
     ], dtype=jnp.float64)
 
     # transform HMC params into model params
-    theta = transform_hmc_to_model(theta_hmc, lower_bounds, upper_bounds)
+    theta = hmc_pos_to_model_pos(theta_hmc, lower_bounds, upper_bounds)
 
     # call func for error/potential
     U = get_potential(theta)
-    
-    # convert potential to logdensity
-    log_density_model = -0.5 * U 
 
-    # transform logdensity of model into hmc logdensity 
-    abs_det_of_transform = jnp.log10(jnp.sum((upper_bounds - lower_bounds) * logit_inv_jac(theta_hmc)))
-    log_density_hmc = log_density_model * abs_det_of_transform
+    # convert potential to logdensity
+    log_density_model = -0.5 * U
+
+    # transform logdensity of model into hmc logdensity
+    log_density_hmc = logdens_model_to_logdens_hmc(
+                        log_density_model,
+                        theta_hmc,
+                        lower_bounds,
+                        upper_bounds
+    )
 
     return log_density_hmc
 
-# we wrap in a lambda for blackjax 
-logdensity = lambda x: logdensity_fn(**x)
 
-# TODO: probably move these
-error_history = np.empty(n_iter+n_warmup_steps, dtype=np.float64)
-rpwp_history = np.empty((n_iter+n_warmup_steps, n_rpbins), dtype=np.float64)
-step_index = np.zeros(1, dtype="i")
+# we wrap as one input for blackjax
+def logdensity(x):
+    return logdensity_fn(**x)
 
-# method to calculate quenched and unquenched rpwp 
+
+# method to calculate rpwp
 # this is what we pure_callback to; it returns both value and gradient
 def potential(
     theta,
@@ -238,34 +224,37 @@ def potential(
     mass_bin_low=mass_bin_edges[0], mass_bin_high=mass_bin_edges[1],
     rpbins=rpbins,
     zmax=zmax,
-    boxsize=box_length,
-    error_history=error_history,
-    rpwp_history=rpwp_history
+    boxsize=box_length
 ):
 
-    # broadcast continue command to other ranks 
+    # broadcast continue command to other ranks
     cont = True
     cont = COMM.bcast(cont, root=0)
 
     # broadcast theta
     COMM.Bcast([theta, MPI.DOUBLE], root=0)
 
-    error, error_grad, rpwp = mse_rpwp(
-            rpwp_goal,
-            log_halomass, log_host_halomass, log_vmax_by_vmpeak,
-            halo_x, halo_y, halo_z,
-            upid, inside_subvol,
-            idx_to_deposit,
-            rpbins, mass_bin_low, mass_bin_high, zmax, box_length,
-            theta
+    error, error_grad = mse_rpwp(
+            rpwp_goal=rpwp_goal,
+            logmpeak=log_halomass,
+            loghost_mpeak=log_host_halomass,
+            log_vmax_by_vmpeak=log_vmax_by_vmpeak,
+            halo_x=halo_x,
+            halo_y=halo_y,
+            halo_z=halo_z,
+            upid=upid,
+            inside_subvol=inside_subvol,
+            idx_to_deposit=idx_to_deposit,
+            rpbins=rpbins,
+            mass_bin_low=mass_bin_low,
+            mass_bin_high=mass_bin_high,
+            zmax=zmax,
+            boxsize=box_length,
+            theta=theta
     )
 
-    # rank zero update our arrays
-    error_history[step_index[0]] = error
-    rpwp_history[step_index[0]] = rpwp
-    step_index[0] = step_index[0]+1
-
     return error, error_grad
+
 
 # this returns only val
 @custom_vjp
@@ -278,7 +267,8 @@ def get_potential(
                     theta
     )
 
-    return val 
+    return val
+
 
 def vjp_fwd(
     theta
@@ -292,24 +282,25 @@ def vjp_fwd(
 
     return val, grad
 
+
 def vjp_bwd(grad, tan):
     # note that blackjax expects a tuple here
     return (grad * tan,)
 
+
 get_potential.defvjp(vjp_fwd, vjp_bwd)
 
 # this is where we split up the ranks
+# sampler set up
 
-# sampler set up 
-
-# non 0 mpi ranks
+# non 0 mpi ranks just do computation
 if RANK > 0:
     while True:
 
         # check condition
         cont = -1
         cont = COMM.bcast(cont, root=0)
-        if not cont: 
+        if not cont:
             break
 
         # receive current theta
@@ -317,14 +308,23 @@ if RANK > 0:
         COMM.Bcast([theta, MPI.DOUBLE], root=0)
 
         # do computation
-        _, _, _ = mse_rpwp(
-                rpwp_goal,
-                halos["logmpeak"], halos["loghost_mpeak"], halos["logvmax_frac"],
-                halos["halo_x"], halos["halo_y"], halos["halo_z"],
-                halos["upid"], halos["_inside_subvol"],
-                idx_to_deposit,
-                rpbins, mass_bin_edges[0], mass_bin_edges[1], zmax, box_length,
-                theta
+        _, _ = mse_rpwp(
+                rpwp_goal=rpwp_goal,
+                logmpeak=halos["logmpeak"],
+                loghost_mpeak=halos["loghost_mpeak"],
+                log_vmax_by_vmpeak=halos["logvmax_frac"],
+                halo_x=halos["halo_x"],
+                halo_y=halos["halo_y"],
+                halo_z=halos["halo_z"],
+                upid=halos["upid"],
+                inside_subvol=halos["_inside_subvol"],
+                idx_to_deposit=idx_to_deposit,
+                rpbins=rpbins,
+                mass_bin_low=mass_bin_edges[0],
+                mass_bin_high=mass_bin_edges[1],
+                zmax=zmax,
+                boxsize=box_length,
+                theta=theta
         )
 
     if RANK == 1:
@@ -339,27 +339,27 @@ else:
 
     # adapt the mass matrix
     initial_position = {
-        "smhm_logm_crit":initial_hmc_params[0],
-        "smhm_ratio_logm_crit":initial_hmc_params[1],
-        "smhm_k_logm":initial_hmc_params[2],
-        "smhm_lowm_index":initial_hmc_params[3],
-        "smhm_highm_index":initial_hmc_params[4],
-        "smhm_sigma_low":initial_hmc_params[5],
-        "smhm_sigma_high":initial_hmc_params[6],
-        "smhm_sigma_logm_pivot":initial_hmc_params[7],
-        "smhm_sigma_logm_width":initial_hmc_params[8],
-        "satmerg_logmhost_crit":initial_hmc_params[9],
-        "satmerg_logmhost_k":initial_hmc_params[10],
-        "satmerg_logvr_crit_dwarfs":initial_hmc_params[11],
-        "satmerg_logvr_crit_clusters":initial_hmc_params[12],
-        "satmerg_logvr_k":initial_hmc_params[13]
+        "smhm_logm_crit" : initial_hmc_params[0],
+        "smhm_ratio_logm_crit" : initial_hmc_params[1],
+        "smhm_k_logm" : initial_hmc_params[2],
+        "smhm_lowm_index" : initial_hmc_params[3],
+        "smhm_highm_index" : initial_hmc_params[4],
+        "smhm_sigma_low" : initial_hmc_params[5],
+        "smhm_sigma_high" : initial_hmc_params[6],
+        "smhm_sigma_logm_pivot" : initial_hmc_params[7],
+        "smhm_sigma_logm_width" : initial_hmc_params[8],
+        "satmerg_logmhost_crit" : initial_hmc_params[9],
+        "satmerg_logmhost_k" : initial_hmc_params[10],
+        "satmerg_logvr_crit_dwarfs" : initial_hmc_params[11],
+        "satmerg_logvr_crit_clusters" : initial_hmc_params[12],
+        "satmerg_logvr_k" : initial_hmc_params[13]
     }
-        
+
     # split the key
     rng_key, sample_key, warmup_key = jax.random.split(rng_key, 3)
 
     print("Begin warmup", flush=True)
-    warmup = blackjax.window_adaptation(blackjax.nuts, logdensity, 
+    warmup = blackjax.window_adaptation(blackjax.nuts, logdensity,
                                         is_mass_matrix_diagonal=False)
     t0 = time.time()
     (init_state, tuned_params), _ = warmup.run(warmup_key, initial_position,
@@ -411,18 +411,18 @@ else:
     print("opt done", flush=True)
 
     samples = states.position
-    print(states.logdensity)
+    print("logdensity:", states.logdensity)
 
-# 4) save output and plot results (rank 0 for IO, everyone for calculations)
+# 4) save output and plot results (rank 0 for IO, all for calculations)
 
-# save outputs 
+# save outputs
 if RANK == 0:
     # positions
     positions = states.position
-        
+
     positions_df = pd.DataFrame.from_dict(positions)
     # need to transform from HMC positions to model positions
-    for p,key in enumerate(positions_df.keys()):
+    for p, key in enumerate(positions_df.keys()):
         positions_df[key] = transform_hmc_to_model(np.array(positions_df[key]),
                                                    lower_bounds[p], upper_bounds[p])
 
@@ -438,56 +438,8 @@ if RANK == 0:
     logdensity_df.insert(0, column="logdensity", value=logdensities)
 
     fpath_logdensity = outdir+"/logdensity_grad.csv"
-    logdensity_df.to_csv(fpath_logdensity, index=False)    
+    logdensity_df.to_csv(fpath_logdensity, index=False)
 
-maybe="""
-# rpwp and error at each position; note this involves all ranks
-rpwp_history = np.empty((n_iter, n_rpbins), dtype=np.float64)
-error_history = np.empty(n_iter, dtype=np.float64)
-# loop over positions
-for i in range(n_iter):
-    # broadcast position / theta
-    if RANK == 0:
-        theta = np.array(positions_df.iloc[i], dtype=np.float64)
-    else:
-        theta = np.empty(n_params, dtype=np.float64)
-    COMM.Bcast([theta, MPI.DOUBLE], root=0)    
-
-    # calculate rpwp
-    w, dw = compute_weight_and_jac(
-                halos["logmpeak"],
-                halos["loghost_mpeak"],
-                halos["logvmax_frac"],
-                halos["upid"],
-                idx_to_deposit,
-                mass_bin_edges[0], mass_bin_edges[1],
-                theta
-    )
-
-    rpwp, _ = compute_rpwp(
-                    x1=halos["halo_x"],
-                    y1=halos["halo_y"],
-                    z1=halos["halo_z"],
-                    w1=w,
-                    w1_jac=dw,
-                    inside_subvol=halos["_inside_subvol"],
-                    rpbins=rpbins,
-                    zmax=zmax,
-                    boxsize=box_length
-    )
-
-    # calculate error and save
-    if RANK == 0:
-        error = np.sum((rpwp-rpwp_goal)*(rpwp-rpwp_goal)) / len(rpwp)
-
-        rpwp_history[i,:] = rpwp
-        error_history[i] = error
-
-# rank 0 write to disk
-if RANK == 0:
-    rpwp_history.tofile(outdir+"/rpwp_history.csv", sep=",")
-    error_history.tofile(outdir+"/error_history.csv", sep=",")
-"""
 
 # let's plot rpwp before and after optimization
 # first we need initial and final rpwp measurements
@@ -544,7 +496,7 @@ rpwp_final, _ = compute_rpwp(
 
 # figure
 if RANK == 0:
-    fig = plt.figure(figsize=(16,8), facecolor="w")
+    fig = plt.figure(figsize=(16, 8), facecolor="w")
 
     plt.plot(rpbins[:-1], rpwp_init, c="tab:blue", linewidth=3)
     plt.plot(rpbins[:-1], rpwp_final, c="tab:orange", linewidth=3)
@@ -556,5 +508,4 @@ if RANK == 0:
 
     plt.legend(["initial guess", "final guess", "ideal"])
 
-    plt.savefig("rpwp_demo_nuts.png")
-
+    plt.savefig("figures/rpwp_demo_nuts.png")
