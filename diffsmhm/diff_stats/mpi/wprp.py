44713b4c7d87ed691c2cf8d133cdb376cea06e0f
Joseph Wick <jwick@polaris-login-04.hsn.cm.polaris.alcf.anl.gov>
2024-05-23 21:29:56 +0000

    Handle cupy inputs

--- diffsmhm/diff_stats/mpi/wprp.py
+++ diffsmhm/diff_stats/mpi/wprp.py
@@ -91,6 +91,13 @@ def wprp_mpi_comp_and_reduce(
     with time_step("final post-processing"):
         # now do norm by RR and compute proper grad
         if RANK == 0:
+            # need be convert some things to numpy from cupy
+            try: 
+                rpbins_squared = np.array(rpbins_squared)
+            except:
+                rpbins_squared = np.array(rpbins_squared.get())
+            print("RRRRRR:", type(rpbins_squared))
+
             n_eff = w_tot**2 / w2_tot
 
             # this is the volume of the shell
