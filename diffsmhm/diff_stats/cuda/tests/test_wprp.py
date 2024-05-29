0cdd587e07919456f654ebe86c21d33a42375715
Joseph Wick <jwick@polaris-login-04.hsn.cm.polaris.alcf.anl.gov>
2024-05-29 23:06:58 +0000

    fix concat

--- diffsmhm/diff_stats/cuda/tests/test_wprp.py
+++ diffsmhm/diff_stats/cuda/tests/test_wprp.py
@@ -23,7 +23,7 @@ from .conftest import SKIP_CUDA_TESTS
 def test_wprp_serial_cuda_smoke():
     data = gen_mstar_data(seed=42)
     bins = np.logspace(0.1, 15, 10)
-    bins = np.concatenate(np.array([0.0]), bins)
+    bins = np.concatenate([np.array([0.0]), bins], dtype=np.float64))
 
     nrp = data["rp_bins"].shape[0] - 1
     wprp, wprp_grad = wprp_serial_cuda(
@@ -53,7 +53,7 @@ def test_wprp_serial_cuda_smoke():
 def test_wprp_serial_cuda():
     data = gen_mstar_data(seed=42)
     bins = np.logspace(0.1, 15, 10)
-    bins = np.concatenate(np.array([0.0]), bins)
+    bins = np.concatenate([np.array([0.0]), bins], dtype=np.float64)
 
     wprp_cuda, wprp_grad_cuda = wprp_serial_cuda(
         x1=data["x"],
