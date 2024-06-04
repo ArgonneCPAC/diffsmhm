f2eae1c677258679ba1494da1dedcf8fd6f5bb30
Joseph Wick <jwick@x3006c0s1b1n0.hsn.cm.polaris.alcf.anl.gov>
2024-06-04 21:46:21 +0000

    add named tuple for Sigma mpi kernel

--- diffsmhm/diff_stats/mpi/types.py
+++ diffsmhm/diff_stats/mpi/types.py
@@ -4,3 +4,8 @@ WprpMPIData = namedtuple(
     "WprpMPIData",
     ["dd", "dd_jac", "w_tot", "w2_tot", "ww_jac_tot", "w_jac_tot"]
 )
+
+SigmaMPIData = namedtuple(
+    "SigmaMPIData",
+    ["sigma", "sigma_grad_1st", "w_tot", "w_jac_tot"]
+)
