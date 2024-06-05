e2742e6eac4f9cecfea55118896793c42f8cad9a
Joseph Wick <jwick@polaris-login-01.hsn.cm.polaris.alcf.anl.gov>
2024-06-05 17:24:39 +0000

    add rpbins field to wprp tuple

--- diffsmhm/diff_stats/mpi/types.py
+++ diffsmhm/diff_stats/mpi/types.py
@@ -2,7 +2,7 @@ from collections import namedtuple
 
 WprpMPIData = namedtuple(
     "WprpMPIData",
-    ["dd", "dd_jac", "w_tot", "w2_tot", "ww_jac_tot", "w_jac_tot"]
+    ["dd", "dd_jac", "w_tot", "w2_tot", "ww_jac_tot", "w_jac_tot", "rpbins_squared"]
 )
 
 SigmaMPIData = namedtuple(
