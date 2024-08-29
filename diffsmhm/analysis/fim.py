bb76b60e3d611ce1fa333b7e7b8b23a1ce6da0f1
Joseph Wick <jwick@polaris-login-02.hsn.cm.polaris.alcf.anl.gov>
2024-08-29 20:33:39 +0000

    add docstring

--- diffsmhm/analysis/fim.py
+++ diffsmhm/analysis/fim.py
@@ -3,7 +3,24 @@ import numpy as np
 
 def make_FIM(logdensity_grad, theta, eps=1e-6):
     """
-    make fisher info matrix given a logdensity fns grad and position
+    Make fisher info matrix given a logdensity function's gradient and position
+
+    Parameters
+    ----------
+    logdensity_grad : function handle
+        Function for the gradient of logdensity. Takes one argument, theta.
+    theta : array_like, shape (n_params,)
+        Position to compute second derivative around.
+    eps : float
+        Finite difference parameter.
+
+    Returns
+    -------
+    FIM : array_like, shape (n_params, n_params)
+        Fisher information matrix around  `theta`.
+    min_eig : float
+        Square root of the minimum eigenvalue of FIM, can be used as an
+        initial estimate of step size in HMC.
     """
 
     n_params = len(theta)
@@ -31,8 +48,7 @@ def make_FIM(logdensity_grad, theta, eps=1e-6):
         print("FIM is not SPD")
         exit()
 
-    # print smallest eig as starting place for step size
+    # smallest eig as a rough starting place for HMC step size
     eigs = np.linalg.eig(FIM)[0]
-    print("min eig of FIM:", np.sqrt(np.min(eigs)), flush=True)
 
-    return FIM
+    return FIM, np.sqrt(np.min(eigs))
